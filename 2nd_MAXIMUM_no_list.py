aa15dd4edafe82525c78a28a9d6d5bfcec675190
sharda suman <83698402+sharda2001@users.noreply.github.com>
2021-06-29 16:25:37 +0530

    Update 2nd_MAXIMUM_no_list.py

--- 2nd_MAXIMUM_no_list.py
+++ 2nd_MAXIMUM_no_list.py
@@ -1,12 +1,12 @@
 num=[10,20,30,40,50,60]
 i=0
 max=0
-sec=0
+sec1=0
 while i<len(num):
 	if  num[i]>max:
-		sec=max
+		sec1=max
 		max=num[i]
-	if sec<num[i] and num[i]<max:
-		sec=num[i]
+	if sec1<num[i] and num[i]<max:
+		sec1=num[i]
 	i=i+1
-print(sec)
+print(sec1)
